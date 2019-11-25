8cce3b9fe33360e492517d249c3f28bc62422f55
FeliPellissari <51676456+FeliPellissari@users.noreply.github.com>
2019-11-25 17:40:08 -0300

    a

---
+++ resources/views/relatorio/carteira.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<style type="text/css">
+		body{
+			background: red;
+		}
+	</style>
+	<title></title>
+</head>
+<body>
+nipnnn	nm  kpkpok k 
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('busca-resultado-avancado',['uses'=> 'BuscaController@buscaAvancadaLi
 Route::get('/busca-avancada-externo',['uses'=> 'BuscaController@buscaExterna', 'as' =>'buscaAvancada']);
 // Route::get('/busca-aberta',['uses'=> 'BuscaController@buscaExternaSimples', 'as' =>'busca']);
 
+Route::get('carteira', 'PDFController@carteira');
+
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
